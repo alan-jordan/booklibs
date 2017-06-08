bfcbc86eb4709b4942a12f8ec1f804c844930b68
Alan Jordan <alanpjordan@gmail.com>
2017-06-08 15:33:44 +1200

    End page basic styling done too

--- public/bundle.js
+++ public/bundle.js
@@ -12259,7 +12259,7 @@ exports.default = Page;
 
 
 Object.defineProperty(exports, "__esModule", {
-   value: true
+  value: true
 });
 
 var _react = __webpack_require__(5);
@@ -12269,11 +12269,20 @@ var _react2 = _interopRequireDefault(_react);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var TheEnd = function TheEnd() {
-   return _react2.default.createElement(
-      "div",
-      { className: "end" },
-      "The End!"
-   );
+  return _react2.default.createElement(
+    'div',
+    { className: 'cover' },
+    _react2.default.createElement('div', { className: 'spine' }),
+    _react2.default.createElement(
+      'div',
+      { className: 'page' },
+      _react2.default.createElement(
+        'h1',
+        { className: 'end' },
+        'The End.'
+      )
+    )
+  );
 };
 
 exports.default = TheEnd;
