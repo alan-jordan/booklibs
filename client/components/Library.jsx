49b161bb663df4e6317afe3817b041b9d61aed3a
Alan Jordan <alanpjordan@gmail.com>
2017-06-08 15:37:05 +1200

    Library template page done

--- client/components/Library.jsx
+++ client/components/Library.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Library = () =>
    (
-      <div className="library">
-         Library view!
-      </div>
+     <div className='cover'>
+       <div className='spine'>
+       </div>
+       <div className='page'>
+         <h2>Pick a story</h2>
+         <ul>
+           <li><a href='#'>Story one</a></li>
+         </ul>
+       </div>
+     </div>
    )
 
 
