44f7a4354d20499012ee126a6e9fb9f8dd8a82f3
Alan Jordan <alanpjordan@gmail.com>
2017-06-08 15:52:57 +1200

    Adding basic fonts

--- client/components/Page.jsx
+++ client/components/Page.jsx
@@ -6,10 +6,10 @@ const Page = () =>
        <div className='spine'>
        </div>
        <div className='page'>
-         <p>This is a story _______.
-           Someone did _______
-           and then _______.
-           Because _________.
+         <p>This is a story <input name='field1' type='text'/>.
+           Someone did <input name='field2' type ='text'/>
+           and then <input name='field3' type='text'/>.
+           Because <input name='field4' type='text'/>.
          </p>
          <div className='images'>
            <div className='image'>
